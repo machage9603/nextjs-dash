aa3e62406a442365a4a4e411e8578f67e7bb033e
machage9603 <mikemachage@gmail.com>
2024-09-06 07:53:37 +0300

    stream Ui

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,19 +1,11 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
+import { fetchRevenue } from '@/app/lib/data';
 
-// This component is representational only.
-// For data visualization UI, check out:
-// https://www.tremor.so/
-// https://www.chartjs.org/
-// https://airbnb.io/visx/
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+  export default async function RevenueChart() { // Make component async, remove the props
+    const revenue = await fetchRevenue(); // Fetch data inside the component
   const chartHeight = 350;
 
    const { yAxisLabels, topLabel } = generateYAxis(revenue);
@@ -35,6 +27,7 @@ export default async function RevenueChart({
             style={{ height: `${chartHeight}px` }}
           >
             {yAxisLabels.map((label) => (
+
               <p key={label}>{label}</p>
             ))}
           </div>
